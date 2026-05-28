a88a58b8b910a525974766f0019e87db11f5174b
Nicorivera2 <nico.riveraf@duocuc.cl>
2026-05-28 01:14:34 -0400

    fix: correcion url base

--- front_despacho/src/constants/BACKEND_IP.js
+++ front_despacho/src/constants/BACKEND_IP.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://10.0.8.73";
+export const BASE_URL = "http://98.93.73.204";
 export const VENTA_API = `${BASE_URL}:8080/api/v1/ventas`;
 export const DESPACHO_API = `${BASE_URL}:8081/api/v1/despachos`;
